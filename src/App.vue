8e20c1f5d674c4b59f9ab63ab82caf4293315252
Juan Casas <imhouses19@gmail.com>
2018-12-09 01:25:35 -0600

    Feature: App initial configuration.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main-component msg="Vue.js Todo list powered by Firebase"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainComponent from './components/MainComponent.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MainComponent
   }
 }
 </script>
