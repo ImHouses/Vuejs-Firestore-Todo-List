6fbcb68a9ae07eae09c583c487119aa4cf2089e2
Juan Casas <imhouses19@gmail.com>
2018-12-10 18:59:45 -0600

    Changed: Removed firebase keys from repository.

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,7 @@
 import { firebase } from "@firebase/app";
 import "@firebase/firestore";
+import { config } from './firebaseConfiguration';
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAXxVNYTrVM4YhG_I-jZU3BhUfNGrwflfA",
-  authDomain: "fire-vue-todo-list.firebaseapp.com",
-  databaseURL: "https://fire-vue-todo-list.firebaseio.com",
-  projectId: "fire-vue-todo-list",
-  storageBucket: "fire-vue-todo-list.appspot.com",
-  messagingSenderId: "526297521212"
-})
-
+const firebaseApp = firebase.initializeApp(config);
 export const db = firebaseApp.firestore();
 db.settings({timestampsInSnapshots: true})
